23f70a653349e80df649a338aa65875330a799dd
Sidharth <shrama@stash.gfk.com>
2017-10-16 15:43:14 +0200

    Type correction

--- hdfsWrite.py
+++ hdfsWrite.py
@@ -2,7 +2,7 @@ import sys
 import os
 import subprocess
 
-hdfsFile = "/sidharth/test/bible1.txt"
+hdfsFile = "/sidharth/test/bible.txt"
 localFile = "/home/hduser/Code/outputFile.txt"
 cat = subprocess.Popen(["hadoop", "fs", "-put", localFile, hdfsFile], stdout=subprocess.PIPE)
 cat.communicate()
